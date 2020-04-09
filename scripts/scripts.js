721ed82d035fd950eec705c9b8201090572bbbff
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-09 12:10:03 -0400

    Add player and computer scores at the end of each round and at the end of each game

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,6 @@
+let playerWinCount = 0;
+let computerWinCount = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
@@ -10,12 +13,9 @@ function computerPlay() {
     let selectPlay = getRandomInt(3);
 
     switch (selectPlay) {
-        case 0:
-            return "rock";
-        case 1:
-            return "paper";
-        case 2:
-            return "scissors";
+        case 0: return "rock";
+        case 1: return "paper";
+        case 2: return "scissors";
     }
 }
 
@@ -24,24 +24,30 @@ function playRound(playerSelection, computerSelection) {
         if (computerSelection === "rock") {
             return "You tied! Rock nullifies rock!"
         } else if (computerSelection === "paper") {
+            computerWinCount++;
             return "You lose! Paper beats rock!"
         } else {
+            playerWinCount++;
             return "You win! Rock beats scissors!"
         }
     }
     if (playerSelection === "paper") {
         if (computerSelection === "rock") {
+            playerWinCount++;
             return "You win! Paper beats rock!"
         } else if (computerSelection === "paper") {
             return "You tied! Paper nullifies paper!"
         } else {
+            computerWinCount++;
             return "You lose! Scissors beat rock!"
         }
     }
     if (playerSelection === "scissors") {
         if (computerSelection === "rock") {
+            computerWinCount++;
             return "You lose! Scissors beats rock!"
         } else if (computerSelection === "paper") {
+            playerWinCount++;
             return "You win! Scissors beats paper!"
         } else {
             return "You tied! Scissors nullfies scissors!"
@@ -49,10 +55,24 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
-function game() {
-
+function game(rounds) {
+    for (let i = 0; i < rounds; i++) {
+        const playerSelection = getPlayerSelection();
+        const computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+        console.log(`Player score: ${playerWinCount} Computer score: ` +
+                `${computerWinCount}`);    
+    }
+    
+    if (playerWinCount > computerWinCount) {
+        console.warn(`You've won the best of ${rounds} games! ` + 
+                `Final score - Player: ${playerWinCount} | Computer: ${computerWinCount}` )
+    } else if (playerWinCount === computerWinCount) {
+        console.warn(`You tied. Final score - Player: ${playerWinCount} | Computer: ${computerWinCount}`)
+    } else {
+        console.warn(`You've lost the best of ${rounds} games. ` + 
+                `Final score - Player: ${playerWinCount} | Computer: ${computerWinCount}` )
+    }
 }
 
-const playerSelection = getPlayerSelection();
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+game(5);
