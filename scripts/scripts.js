17269920b8deb0549b4552b0a33c07166a5d89fb
Joe Webster <joseph.scott.webster@gmail.com>
2020-04-15 14:35:08 -0400

    Complete fully functional rock paper scissors ui

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,37 @@
+const content = document.querySelector('.content');
 const message = document.querySelector('.message');
+const choices = document.querySelector('.choices');
+const playerScore = document.querySelector('.player-score');
+const compScore = document.querySelector('.comp-score');
+const roundNum = document.querySelector('.round');
+const playAgain = document.createElement('button');
+playAgain.innerText = "Play again?"
+playAgain.classList.add('play-again');
+
 
 let playerWinCount = 0;
 let computerWinCount = 0;
+let roundCount = 0;
 
 window.addEventListener('click', (e) => {
+    
     const playerSelection = e.target.dataset.choice;
     const computerSelection = computerPlay();
-                    
+    if (!playerSelection) return;
+
     playRound(playerSelection, computerSelection);
-    console.log(showRoundResults());    
+    increaseRoundCount();
+    
+    if (roundCount === 5) {
+        showGameResults();
+    }
 });
 
+
+function increaseRoundCount() {
+    roundCount++;
+    roundNum.textContent = roundCount;
+}
 
 function computerPlay() {
     const choices = ["rock", "paper", "scissors"];
@@ -45,11 +66,13 @@ function playRound(playerSelection, computerSelection) {
 //Determine text for individual round results
 function win(playerSelection, computerSelection) {
     playerWinCount++;
+    playerScore.innerText = playerWinCount;
     message.innerText = `You won! ${capitalize(playerSelection)} beats ${computerSelection}!`;    
 }
 
 function lose(playerSelection, computerSelection) {
     computerWinCount++;
+    compScore.innerText = computerWinCount;
     message.innerText = `You lost... ${capitalize(playerSelection)} loses to ${computerSelection}.`;
 }
 
@@ -63,15 +86,17 @@ function showRoundResults() {
 }
 
 //Display game's final score results
-function showGameResults(rounds) {
+function showGameResults() {
+    choices.style.display = 'none';
+    content.appendChild(playAgain);
+    // playAgain.style.display = 'inline-block';
+    
     if (playerWinCount > computerWinCount) {
-        return `You've won the best of ${rounds} games! ` + 
-                `Final score - Player: ${playerWinCount} | Computer: ${computerWinCount}`;
+        message.innerText = `Congrats! You've won the best of ${roundCount} games!`;
     } else if (playerWinCount === computerWinCount) {
-        return `You tied. Final score - Player: ${playerWinCount} | Computer: ${computerWinCount}`;
+        message.innerText = `Womp womp. After ${roundCount} rounds, you tied the computer.`;
     } else {
-        return `You've lost the best of ${rounds} games. ` + 
-                `Final score - Player: ${playerWinCount} | Computer: ${computerWinCount}`;
+        message.innerText = `You've lost the best of ${roundCount} games. Better luck next time.`;
     }
 }
 
@@ -80,3 +105,22 @@ function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+playAgain.addEventListener('click', () => {
+    playerWinCount = 0;
+    computerWinCount = 0;
+    roundCount = 0;
+    
+    playerScore.innerText = playerWinCount;
+    compScore.innerText = computerWinCount;
+    roundNum.innerText = roundCount;
+
+    choices.style.display = 'flex';
+    message.innerText = "Choose your weapon:"
+
+    content.removeChild(playAgain);
+
+    
+
+
+
+});
